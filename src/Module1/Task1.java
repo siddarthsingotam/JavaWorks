268437860cf66d0813cfc1357492d45855e33796
Siddarth <siddarthsingotam1213@gmail.com>
2023-10-23 13:54:59 +0300

    First commit

---
+++ src/Module1/Task1.java
@@ -0,0 +1,8 @@
+package Module1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Siddarth!");
+        System.out.println("Hello Siddarth second line!");
+    }
+}
---
+++ src/Module1/Task2.java
@@ -0,0 +1,5 @@
+package Module1;
+
+public class Task2 {
+
+}
