eebb840c5af525c6136c6ca8ea8d3c31b9917cdf
Siddarth <siddarthsingotam1213@gmail.com>
2023-10-30 16:22:39 +0200

    Module 1 Part 3 Task 1 completed

---
+++ src/Module1/Part3/Task1.java
@@ -0,0 +1,49 @@
+package Module1.Part3;
+
+import java.util.Scanner;
+
+public class Task1 {
+    private static double disc(double a, double b, double c){
+        double D = Math.pow(b, 2) - 4*a*c;
+
+        return D;
+    }
+
+    private static boolean checkRoots(double discriminant) {
+        return discriminant >= 0;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter the first coefficient a: ");
+        double a = Double.parseDouble(reader.nextLine());
+
+        System.out.println("Enter the first coefficient b: ");
+        double b = Double.parseDouble(reader.nextLine());
+
+        System.out.println("Enter the first coefficient c: ");
+        double c = Double.parseDouble(reader.nextLine());
+
+        // Calculating Discriminant
+        double D = disc(a, b, c);
+
+        // Checking roots exist
+        boolean response = checkRoots(D);
+
+
+        if (response){
+            double root1 = (-b + Math.sqrt(D))/2*a;
+            double root2 = (-b - Math.sqrt(D))/2*a;
+
+            System.out.println("\nThe roots exists!");
+            System.out.println("roots: " + root1 + " ," + root2);
+        }
+
+        else {
+            System.out.println("No roots!");
+        }
+
+    }
+}
---
+++ src/Module1/Part3/himan.java
@@ -0,0 +1,33 @@
+package Module1.Part3;
+
+import java.util.Scanner;
+
+public class himan {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double rootA, rootB;
+
+        System.out.println("Provide coefficient a: ");
+        double coffA = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Provide coefficient b: ");
+        double coffB = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Provide coefficient c: ");
+        double coffC = Double.parseDouble(scanner.nextLine());
+
+        double rootTerm = (Math.pow(coffB, 2) - (4 * coffA * coffC));
+
+        if (rootTerm < 0) {
+            System.out.println("The equation has no real solutions");
+        } else if (rootTerm == 1) {
+            rootA = (-coffB + rootTerm) / (2 * coffA);
+            rootB = (-coffB - rootTerm) / (2 * coffA);
+            System.out.printf("x=%.2f , x=%.2f", rootA, rootB);
+        } else {
+            rootA = (-coffB + Math.sqrt(rootTerm)) / (2 * coffA);
+            rootB = (-coffB - Math.sqrt(rootTerm)) / (2 * coffA);
+            System.out.printf("x=%.2f , x=%.2f", rootA, rootB);
+        }
+    }
+}
