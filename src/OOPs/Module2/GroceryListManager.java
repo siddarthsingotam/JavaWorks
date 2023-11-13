0d02451b116b4e08435bb86994a4db388d15dd68
Siddarth <siddarthsingotam1213@gmail.com>
2023-11-13 10:46:09 +0200

    Task 4 finished

--- src/OOPs/Module2/GroceryListManager.java
+++ src/OOPs/Module2/GroceryListManager.java
@@ -1,32 +1,44 @@
 package OOPs.Module2;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GroceryListManager {
     private HashMap<String, Double> groceryList = new HashMap<>();
     private HashMap<String, String> itemCategories = new HashMap<>();
+    private HashMap<String, Integer> itemQuantities = new HashMap<>();
 
-    public void addItem(String item, double cost, String category) {
+    public void addItem(String item, double cost, String category, int quantity) {
         if (!groceryList.containsKey(item)) {
             groceryList.put(item, cost);
             itemCategories.put(item, category);
-            System.out.println("Added \"" + item + "\" to the grocery list with a cost of $" + cost + " in the category: " + category);
+            itemQuantities.put(item, quantity);
+            System.out.println("Added \"" + item + "\" to the grocery list with a cost of $" + cost +
+                    " in the category: " + category + " and quantity: " + quantity);
         } else {
             System.out.println("\"" + item + "\" is already in the grocery list.");
         }
     }
 
-    public void addItemWithCategory(String item, String category) {
-        // Assume default cost is 0.0 when adding an item with a category
-        addItem(item, 0.0, category);
+    public void addItemWithQuantity(String item, int quantity) {
+        // Assume default cost is 0.0 and default category is "Uncategorized"
+        addItem(item, 0.0, "Uncategorized", quantity);
+    }
+
+    public void updateQuantity(String item, int newQuantity) {
+        if (groceryList.containsKey(item)) {
+            itemQuantities.put(item, newQuantity);
+            System.out.println("Updated quantity of \"" + item + "\" to " + newQuantity);
+        } else {
+            System.out.println("\"" + item + "\" is not in the grocery list.");
+        }
     }
 
     public void removeItem(String item) {
         if (groceryList.containsKey(item)) {
             groceryList.remove(item);
             itemCategories.remove(item);
+            itemQuantities.remove(item);
             System.out.println("Removing \"" + item + "\" from the list...");
         } else {
             System.out.println("\"" + item + "\" is not in the grocery list.");
@@ -38,7 +50,8 @@ public class GroceryListManager {
         for (Map.Entry<String, Double> entry : groceryList.entrySet()) {
             String item = entry.getKey();
             String category = itemCategories.get(item);
-            System.out.println(item + " - $" + entry.getValue() + " (Category: " + category + ")");
+            int quantity = itemQuantities.get(item);
+            System.out.println(item + " - $" + entry.getValue() + " (Category: " + category + ", Quantity: " + quantity + ")");
         }
     }
 
@@ -48,11 +61,13 @@ public class GroceryListManager {
         return isPresent;
     }
 
-    public void displayByCategory(String category) {
-        System.out.println("Items in Category \"" + category + "\":");
-        for (Map.Entry<String, String> entry : itemCategories.entrySet()) {
-            if (entry.getValue().equalsIgnoreCase(category)) {
-                System.out.println(entry.getKey());
+    public void displayAvailableItems() {
+        System.out.println("Available Items:");
+        for (Map.Entry<String, Integer> entry : itemQuantities.entrySet()) {
+            String item = entry.getKey();
+            int quantity = entry.getValue();
+            if (quantity > 0) {
+                System.out.println(item + " - Quantity: " + quantity);
             }
         }
     }
@@ -60,22 +75,26 @@ public class GroceryListManager {
     public static void main(String[] args) {
         GroceryListManager manager = new GroceryListManager();
 
-        // Add items with costs and categories
-        manager.addItem("Apples", 2.5, "Fruits");
-        manager.addItem("Milk", 1.5, "Dairy");
-        manager.addItem("Bread", 1.0, "Bakery");
+        // Add items with costs, categories, and quantities
+        manager.addItem("Apples", 2.5, "Fruits", 5);
+        manager.addItem("Milk", 1.5, "Dairy", 2);
+        manager.addItem("Bread", 1.0, "Bakery", 3);
 
-        // Add items with categories only (assume default cost is 0.0)
-        manager.addItemWithCategory("Bananas", "Fruits");
-        manager.addItemWithCategory("Cheese", "Dairy");
+        // Add items with quantities only (assume default cost, category, and quantity is 0)
+        manager.addItemWithQuantity("Bananas", 4);
+        manager.addItemWithQuantity("Cheese", 1);
 
         // Display the grocery list
         manager.displayList();
 
-        // Display items in a specific category
-        manager.displayByCategory("Fruits");
+        // Update quantity of an item
+        manager.updateQuantity("Apples", 8);
+
+        // Display available items (quantity > 0)
+        manager.displayAvailableItems();
     }
 }
+
 
 
 
