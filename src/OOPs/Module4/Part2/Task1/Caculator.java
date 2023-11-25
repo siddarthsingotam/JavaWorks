9a47fd00bbaebf7512aeae2b9afafd5922720df0
Siddarth <siddarthsingotam1213@gmail.com>
2023-11-25 23:49:30 +0200

    Task 4.1 done

---
+++ src/OOPs/Module4/Part2/Task1/Caculator.java
@@ -0,0 +1,46 @@
+package OOPs.Module4.Part2.Task1;
+
+// Copilot is not bad, helps me with the cheesy stuff.
+// In a new project, write a Calculator class that has the ability to sum positive integers. A negative integer should throw an exception.
+//
+//The class acts as the model in the MVC pattern. It should have the following methods:
+//
+//A method that resets the calculator to zero.
+//A method that adds an integer to the calculator.
+//A method that returns the current value of the calculator
+public class Caculator {
+    int result = 0;
+    public Caculator() {
+    }
+
+    // Copilot helped me with this to sum positive integers, just wrote the method name.
+    public void addPositiveInteger(int num) {
+        if (num < 0) {
+            System.out.println("Error: Negative integers are not allowed.");
+        } else {
+            result += num;
+        }
+    }
+
+    public void reset() {
+       result = 0;
+    }
+
+
+    // to get current value of calculator - Copilot helped me with this
+    public int getResult() {
+        return result;
+    }
+
+
+    // Made some adjustments in the temporary main method to test the code, also partially written by copilot
+    public static void main(String[] args) {
+        Caculator c = new Caculator();
+        System.out.println(c.getResult()); // should print 0
+        c.addPositiveInteger(2);
+        c.addPositiveInteger(3);
+        System.out.println(c.getResult()); // should print 5
+        c.reset();
+        System.out.println(c.getResult()); // should print 0 again
+    }
+}
